2e9e5c0502b9f56807709535af30b1d998327e9e
liber09 <linda.bergsangel@hotmail.com>
2023-03-18 06:25:35 +0100

    Password strength check

--- static/jsfrontend/register.js
+++ static/jsfrontend/register.js
@@ -1,3 +1,20 @@
+let timeout;
+let password = document.getElementById('PassEntry');
+let strengthBadge = document.getElementById('StrengthDisp');
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+
+password.addEventListener("focusout", () => {
+  strengthBadge.style.display = 'block';
+  clearTimeout(timeout);
+  timeout = setTimeout(() => StrengthChecker(password.value), 500);
+  if(password.value.length !== 0) {
+      strengthBadge.style.display != 'block';
+  } else {
+      strengthBadge.style.display = 'none';
+  }
+});
+
 $(window, document, undefined).ready(function() {
 
     $('input').blur(function() {
@@ -33,4 +50,17 @@ $(window, document, undefined).ready(function() {
     });
   
   });
+
+  function StrengthChecker(PasswordParameter) {
+    if(strongPassword.test(PasswordParameter)) {
+        strengthBadge.style.backgroundColor = "green";
+        strengthBadge.textContent = 'Strong';
+    } else if(mediumPassword.test(PasswordParameter)) {
+        strengthBadge.style.backgroundColor = 'blue';
+        strengthBadge.textContent = 'Medium';
+    } else {
+        strengthBadge.style.backgroundColor = 'red';
+        strengthBadge.textContent = 'Weak';
+    }
+}
 
